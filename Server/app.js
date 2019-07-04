4297cdd298ec872d24bb097a30d7a4dec28df00b
matkaingo <50921004+matkaingo@users.noreply.github.com>
2019-07-04 05:52:23 -0500

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -4,7 +4,7 @@ var http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Registrazo</h1>');
+  res.end('<h1>Registrazosazo</h1>');
 });
 
 server.listen(port,() => {
