51246659a65167040817e7ce4ba8c72034639673
matkaingo <50921004+matkaingo@users.noreply.github.com>
2019-07-04 05:07:54 -0500

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const port = process.env.PORT || 3000
-
+var http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
@@ -10,3 +10,9 @@ const server = http.createServer((req, res) => {
 server.listen(port,() => {
   console.log(`Server running at port `+port);
 });
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(1010);
